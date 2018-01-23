9762a7601e78fd9b7ee2a336c389dbcd5a97b408
July Slavchev <jul.slavhev.tues19@gmail.com>
2018-01-23 22:13:00 +0200

    go radou

--- canvas/script.js
+++ canvas/script.js
@@ -28,20 +28,45 @@ var ball = {
 
 var Weapon = {
   x: 200,
-  y: 450,
+  y: 480,
   width: 150,
   height: 150,
   xv: 0,
   yv: 0,
+	rightPressed: false,
+	leftPressed: false,
   draw: function() {
    ctx.drawImage(gun, this.x, this.y, this.width, this.height); 
   }
+};
+
+var Bullet = {
+	radius: 5,
+	x: Weapon.x + 80,
+	y: Weapon.y,
+	//width: 30,
+	//heigt: 30,
+	xv: 0 ,
+	yv: -5,
+	color: 'red',
+	keyPressed: false,
+	draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  },
+
 };
 
 function draw() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   ball.draw();
   Weapon.draw();
+  Bullet.draw();
+  //Bullet.x += Bullet.xv;
+  Bullet.y += Bullet.yv;
   ball.x += ball.vx;
   ball.y += ball.vy;
   raf = window.requestAnimationFrame(draw);
@@ -51,7 +76,17 @@ function draw() {
   if (ball.x + ball.vx + ball.radius> canvas.width || ball.x + ball.vx - ball.radius < 0) {
     ball.vx = -ball.vx;
   }
-
+	if(Weapon.rightPressed && Weapon.x < canvas.width - Weapon.width) {
+    Weapon.x += 7;
+  }
+  else if(Weapon.leftPressed && Weapon.x > 0) {
+  	Weapon.x -= 7;
+  }
+  if(Bullet.keyPressed) {
+  	Bullet.draw();
+  	Bullet.y += Bullet.yv;
+  	//Bullet.keyPressed = false;
+  }
 }
 
 canvas.addEventListener('mouseover', function(e) {
@@ -62,8 +97,35 @@ canvas.addEventListener('mouseout', function(e) {
   window.cancelAnimationFrame(raf);
 });
 
-canvas.addEventListener('mousemove', function(e){
-  Weapon.x = e.clientX - 30
-});
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
+//canvas.addEventListener('click', weaponFire, false);
+
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        Weapon.rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        Weapon.leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        Weapon.rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        Weapon.leftPressed = false;
+    }
+}
+
+/*function weaponFire(e) {
+	if(e.keyCode == 32) {
+	 Bullet.keyPressed = true;
+	 console.log('ajdee');
+	}
+}
+*/
 ball.draw();
