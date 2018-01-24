9af2bb63445f59152995c8d9a05ae515daee1f5a
rrhubenov <rrhubenov@gmail.com>
2018-01-24 13:12:48 +0200

    Add random color when hit = TRUE

--- canvas/script.js
+++ canvas/script.js
@@ -5,16 +5,18 @@ ctx.canvas.height = window.innerHeight - 50;
 var raf;
 document.body.style.backgroundColor = "white";
 var gun = new Image();
-DrawWeapon = function(){
+var projectile = new Image();
+DrawAssets = function(){
   gun.src = 'https://t1.rbxcdn.com/23678b437e11f8c47fc3b88681c13bc4';  
+  projectile.src = 'https://i.imgur.com/7voTA58.gif';
 }
 
 
-var ball = {
+var Ball = {
   radius: 35,
   x:  Math.floor((Math.random() * 300) + 35),
   y:  Math.floor((Math.random() * 300) + 35),
-  vx: Math.floor((Math.random() * 10) - 10),
+  vx: 5,
   vy: 5,
   color: 'blue',
   draw: function() {
@@ -28,7 +30,7 @@ var ball = {
 
 var Weapon = {
   x: 200,
-  y: 480,
+  y: 450,
   width: 150,
   height: 150,
   xv: 0,
@@ -41,51 +43,69 @@ var Weapon = {
 };
 
 var Bullet = {
-	radius: 5,
-	x: Weapon.x + 80,
-	y: Weapon.y,
-	//width: 30,
-	//heigt: 30,
+	x: Weapon.x + 41,
+	y: Weapon.y + 10,
+	width: 70,
+	height: 70,
 	xv: 0 ,
 	yv: -5,
-	color: 'red',
 	keyPressed: false,
 	draw: function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  },
-
+   ctx.drawImage(projectile, this.x, this.y, this.width, this.height);     
+  }
 };
+
+function getDistance(x1, y1, x2, y2){
+  var x_distance = x2 - x1;
+  var y_distance = y2 - y1;
+
+  return Math.sqrt(Math.pow(x_distance, 2) + Math.pow(y_distance, 2));
+}
 
 function draw() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
-  ball.draw();
+  Ball.draw();
   Weapon.draw();
-  Bullet.draw();
-  //Bullet.x += Bullet.xv;
-  Bullet.y += Bullet.yv;
-  ball.x += ball.vx;
-  ball.y += ball.vy;
+  Ball.x += Ball.vx;
+  Ball.y += Ball.vy;
   raf = window.requestAnimationFrame(draw);
-  if (ball.y + ball.vy + ball.radius> canvas.height || ball.y + ball.vy -ball.radius < 0) {
-    ball.vy = -ball.vy;
+
+  if (Ball.y + Ball.vy + Ball.radius> canvas.height || Ball.y + Ball.vy -Ball.radius < 0) {
+    Ball.vy = -Ball.vy;
   }
-  if (ball.x + ball.vx + ball.radius> canvas.width || ball.x + ball.vx - ball.radius < 0) {
-    ball.vx = -ball.vx;
+  if (Ball.x + Ball.vx + Ball.radius> canvas.width || Ball.x + Ball.vx - Ball.radius < 0) {
+    Ball.vx = -Ball.vx;
   }
+
+  if (getDistance(Ball.x, Ball.y, Bullet.x, Bullet.y) < Ball.radius + Bullet.width/2){
+    if(getDistance(Ball.x, Ball.y, Weapon.x, Weapon.y) > Ball.radius + Weapon.width/2){
+      Bullet.keyPressed = false;
+      Bullet.y = Weapon.y + 10 ;
+      Bullet.x = Weapon.x + 40;
+      Ball.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    }else {
+      alert("YOU GOT HIT!");
+      document.location.reload();
+    }
+  }
+
 	if(Weapon.rightPressed && Weapon.x < canvas.width - Weapon.width) {
     Weapon.x += 7;
+    if(Bullet.y == Weapon.y + 10){
+      Bullet.x +=7;
+    }
   }
   else if(Weapon.leftPressed && Weapon.x > 0) {
-  	Weapon.x -= 7;
+    Weapon.x -= 7;
+    if(Bullet.y == Weapon.y + 10){
+      Bullet.x -= 7;      
+    }
   }
   if(Bullet.keyPressed) {
-  	Bullet.draw();
-  	Bullet.y += Bullet.yv;
-  	//Bullet.keyPressed = false;
+      console.log("asd");
+  	  Bullet.draw();
+      Bullet.y += Bullet.yv;
+      if(Bullet.y < -20)Bullet.keyPressed = false;
   }
 }
 
@@ -100,8 +120,6 @@ canvas.addEventListener('mouseout', function(e) {
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-//canvas.addEventListener('click', weaponFire, false);
-
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -109,6 +127,13 @@ function keyDownHandler(e) {
     }
     else if(e.keyCode == 37) {
         Weapon.leftPressed = true;
+    }
+    else if(e.keyCode == 32) {
+        Bullet.keyPressed = true;
+        if(Bullet.y < 0){
+          Bullet.y = Weapon.y + 10;
+          Bullet.x = Weapon.x + 41;
+        }
     }
 }
 
@@ -121,11 +146,5 @@ function keyUpHandler(e) {
     }
 }
 
-/*function weaponFire(e) {
-	if(e.keyCode == 32) {
-	 Bullet.keyPressed = true;
-	 console.log('ajdee');
-	}
-}
-*/
-ball.draw();
+Ball.draw();
+
